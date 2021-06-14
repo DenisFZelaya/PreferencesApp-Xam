0270be66ca5780e8d1917157f7caf3eb92c36a16
Denis Federico Zelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-06-13 20:01:03 -0600

    Initial commit

---
+++ PreferencesApp/PreferencesApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace PreferencesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+                eNombre.Text = Preferences.Get("pNombre", string.Empty);
+
+                swSesion.IsToggled = Preferences.Get("swSesion", false);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Preferences.Set("swSesion", swSesion.IsToggled);
+            Preferences.Set("pNombre", eNombre.Text);
+        }
+    }
+}
